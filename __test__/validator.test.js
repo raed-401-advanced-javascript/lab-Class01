15ee4de41bc1b65be38946253cafc9c191ad5710
raedeid <raedeidalrfou12345@gmail.com>
2020-01-24 19:33:34 +0200

    end of coding

--- __test__/validator.test.js
+++ __test__/validator.test.js
@@ -5,65 +5,76 @@ const validator = require('../lib/validator.js');
 describe('validator module performs basic validation of', () => {
 
   // TODO: Make this series of tests less repetitive ... DRY it out
+  let str = 'yes';
+  let num = 1;
+  let arr = ['a'];
+  let obj = {x:'y'};
+  let func = () => {};
+  let bool = false;
 
   it('strings', () => {
-    let str = 'yes';
-    let num = 1;
-    let arr = ['a'];
-    let obj = {x:'y'};
-    let func = () => {};
-    let bool = false;
+    let tester = [num,arr,obj,func,bool]
+    for(let i=0;i<tester.length;i++){
+      expect(validator.isString(tester[i])).toBeFalsy();
+    }
     expect(validator.isString(str)).toBeTruthy();
-    expect(validator.isString(num)).toBeFalsy();
-    expect(validator.isString(arr)).toBeFalsy();
-    expect(validator.isString(obj)).toBeFalsy();
-    expect(validator.isString(func)).toBeFalsy();
-    expect(validator.isString(bool)).toBeFalsy();
   });
 
   it('numbers', () => {
-    expect(true).toBeFalsy();
+    expect(validator.isNumber(num)).toBeTruthy();
   });
 
   it('arrays', () => {
-    expect(true).toBeFalsy();
+    expect(!validator.isArray(arr)).toBeFalsy();
   });
 
   it('objects', () => {
-    expect(true).toBeFalsy();
+    expect(!validator.isObject(obj)).toBeFalsy();
   });
 
   it('booleans', () => {
-    expect(true).toBeFalsy();
+    expect(!validator.isBollean(bool)).toBeFalsy();
   });
 
   it('functions', () => {
-    expect(true).toBeFalsy();
+    expect(!validator.isFunction(func)).toBeFalsy();
   });
 
 });
 
 describe('validator module performs complex validations', () => {
+  let object_sample = {
+   a:5,
+   b:'play',
+   c:[1,2,3],
+   d: true,
+   e:{g:'good',d:'khristian best TA'}
+  }
 
   it('validates the presence of required object properties at any level', () => {
     // i.e. does person.hair.color exist and have a good value, not just person.hair
-    expect(true).toBeFalsy();
+    expect(object_sample.e.g?1:0).toBeTruthy();
   });
 
   it('validates the proper types of object properties', () => {
     // i.e. person.name must be a string, etc.
-    expect(true).toBeFalsy();
+    expect(validator.isString( object_sample.b)).toBeTruthy();
   });
 
   it('validates the types of values contained in an array', () => {
     // i.e. an array of all strings or numbers
-    expect(true).toBeFalsy();
+    expect(validator.isArray(object_sample.c)).toBeTruthy();
   });
 
   it('validates a value array against an approved list', () => {
     // i.e. a string might only be allowed to be "yes" or "no"
-    expect(true).toBeFalsy();
+    expect(validator.isBollean(object_sample.d)).toBeTruthy();
   });
+  it('check if the key exsist',()=>{
+    let test = "e"
+    expect(Object.keys(object_sample).includes(test)).toBeTruthy()
+
+  })
 
   // TODO: Cover so, so many more cases
 
